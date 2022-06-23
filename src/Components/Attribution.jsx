bd93d943969305d0bfd91406e7036d44e530c24e
Abdelghafour <ghafourragg@gmail.com>
2022-06-23 20:23:10 +0100

    added utils files, edited the html file, added attribution.jsx, added README files

---
+++ src/Components/Attribution.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Attribution = () => {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/Abdelghafour122"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Your Name Here
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Attribution;
