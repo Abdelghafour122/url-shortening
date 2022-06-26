b111e14c737acdfeca79df446268f53beaf498b4
Abdelghafour <ghafourragg@gmail.com>
2022-06-26 11:47:47 +0100

    made the responsive for buttons & navbar, tweaked the mobile menu

--- src/Routes/Homepage/Navbar.jsx
+++ src/Routes/Homepage/Navbar.jsx
@@ -7,12 +7,15 @@ import MobileMenu from "../../Components/MobileMenu";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const setNoScroll = () => {
+    document.body.classList.toggle("no-scroll");
+  };
   return (
     <nav>
       <div className="container df ai-c jc-sb">
         <div className="left df ai-c jc-sb">
           <img src={logo} alt="Shortly-logo" />
-          <ul className="desktop-links hide-in-mobile">
+          <ul className="desktop-links hide-in-mobile df ai-c jc-c">
             <li>
               <button className="empty">Features</button>
             </li>
@@ -28,14 +31,14 @@ const Navbar = () => {
           <button className="empty">Login</button>
           <button className="rounded">Sign Up</button>
         </div>
-        <button className="hide-in-desktop">
-          <img
-            src={open ? close : hamMenu}
-            alt="Open mobile links menu"
-            onClick={() => {
-              setOpen((open) => !open);
-            }}
-          />
+        <button
+          className="ham-menu hide-in-desktop df ai-c jc-c"
+          onClick={() => {
+            setOpen(!open);
+            setNoScroll();
+          }}
+        >
+          <img src={open ? close : hamMenu} alt="Open mobile links menu" />
         </button>
         {open && <MobileMenu />}
       </div>
