92a04687a639b892ad43c6a469a491232a03ca47
Abdelghafour <ghafourragg@gmail.com>
2022-07-02 11:59:40 +0100

    made mobile styles for short list and shorts and inputField

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -1,20 +1,22 @@
-import React, { useMemo } from "react";
+import React from "react";
 
 const InputField = ({ link, setLink, handleClick, validLink }) => {
   return (
-    <div className="input-holder">
-      <input
-        className={`${!validLink && "err-input"}`}
-        type="text"
-        name="address"
-        placeholder="Shorten a link here..."
-        value={link}
-        onChange={(e) => setLink(e.target.value)}
-      />
+    <div className="input-holder df ai-c jc-c">
+      <div className="input">
+        <input
+          className={`${!validLink && "err-input"}`}
+          type="text"
+          name="address"
+          placeholder="Shorten a link here..."
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+        />
+        {!validLink && <p className="err">Please add a link</p>}
+      </div>
       <button className="squared big" onClick={handleClick}>
         Shorten It!
       </button>
-      {!validLink && <p className="err">Please add a link</p>}
     </div>
   );
 };
--- src/Routes/Homepage/LinksList.jsx
+++ src/Routes/Homepage/LinksList.jsx
@@ -3,7 +3,7 @@ import ShortenedLink from "../../Components/ShortenedLink";
 
 const LinksList = ({ linksList }) => {
   return (
-    <div className="links-list">
+    <div className="links-list df ai-c jc-c">
       {linksList.map((link, ind) => {
         return (
           <ShortenedLink
